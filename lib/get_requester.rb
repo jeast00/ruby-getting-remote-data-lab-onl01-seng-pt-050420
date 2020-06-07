f4782c6d17fee66969ac6cb64a98ad6acfd3274d
Jonathan East <jonathan_east@ymail.com>
2020-06-07 17:18:39 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,12 +6,11 @@ require 'open-uri'
 class GetRequester #define constant class
 
 
-  # def initialize(url) #define an 'initialize' method passing in a URL
-  #   @url = url
-  # end
+  def initialize(url) #define an 'initialize' method passing in a URL
+  end
 
   # Setting up the GET request
-  def get_response_body(url)
+  def get_response_body
     uri = URI.parse(url) #parse the url being passed in with 'URI.parse'
     response = NET::HTTP.get_response(uri) # 'GET' the response of the uri with 'NET::HTTP.get_resonse' and pass in the uri variable
     response.body #return the 'body' from the 'response' variable
