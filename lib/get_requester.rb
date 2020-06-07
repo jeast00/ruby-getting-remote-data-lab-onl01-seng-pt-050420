69a47675d8fdf57d97be4b61d2031e0cfd2d2b16
Jonathan East <jonathan_east@ymail.com>
2020-06-07 17:20:20 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -11,9 +11,9 @@ class GetRequester #define constant class
 
   # Setting up the GET request
   def get_response_body
-    uri = URI.parse() #parse the url being passed in with 'URI.parse'
-    response = NET::HTTP.get_response(uri) # 'GET' the response of the uri with 'NET::HTTP.get_resonse' and pass in the uri variable
-    response.body #return the 'body' from the 'response' variable
+    uri = URI.parse(url) #parse the url being passed in with 'URI.parse'
+    #response = NET::HTTP.get_response(uri) # 'GET' the response of the uri with 'NET::HTTP.get_resonse' and pass in the uri variable
+    #response.body #return the 'body' from the 'response' variable
   end
 
 end
